e6bbd81fc4dab2206d8a1adaaf253ca95ff7828f
Ruben C. Hidalgo <19698793+rhidalgogithub@users.noreply.github.com>
2020-10-12 00:50:03 -0500

    Initial Edits

---
+++ A01_ExpTools/A01_main.cpp
@@ -0,0 +1,14 @@
+//Ruben C. Hidalgo, 2020-10-11 Sunday
+//Initial upload Solution to GitHub
+#include <iostream>
+#include <string>
+#include <Windows.h>
+using namespace std;
+
+int main() {
+	cout << "Hello, GitHub Repository" << endl;
+
+
+	system("pause");
+	return 0;
+}
